d1fd3f9781ca0f4286d749d4bc0f0ffd58208387
soimmary <55881643+soimmary@users.noreply.github.com>
2021-05-25 12:38:50 +0300

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -7,18 +7,9 @@ from math import sqrt
 from tqdm import tqdm
 
 
-'''
-# Вычисляем размер корпуса
 corpora_path = '/Users/mariabocharova/PycharmProjects/Thesis/string_corpora.txt'
 with open(corpora_path, 'r', encoding='utf-8') as f:
     corpora = f.read()
-
-
-# Считаем частоту (ipm) вхождений слова на всем корпусе
-def corpora_freq(term):
-    global corpora, corpora_size
-    term_freq = round(corpora.count(term) / 117000000 * 1000000, 2)
-    return term_freq'''
 
 
 # Считаем частоту (ipm) вхождений слова на всем корпусе
@@ -35,11 +26,11 @@ paths = ['/Users/mariabocharova/PycharmProjects/Thesis/lemmatization/frequency_d
          '/Users/mariabocharova/PycharmProjects/Thesis/lemmatization/fivegram_frequency_dict.csv']
 
 
-'''for path in tqdm(paths):
+for path in tqdm(paths):
     tqdm.pandas()
     df = pd.read_csv(path)
     df['частота (ipm)'] = df['термин'].progress_apply(freq)
-    df.to_csv(f'{path[:-4]}_full.csv')'''
+    df.to_csv(f'{path[:-4]}_full.csv')
 
 
 # Для вычисления коэффициента Жуайна разделим текст на ~ 1 000 одинаковых частей
@@ -48,7 +39,7 @@ def split_corpora(corpora_path):
         i = 1
         slice = []
         for line in f:
-            if len(slice) < 130000:
+            if len(slice) < 1200000:
                 slice.extend(line.split())
             else:
                 with open(f'/Users/mariabocharova/PycharmProjects/Thesis/split_corpora/{i}_part.txt',
@@ -62,35 +53,84 @@ def split_corpora(corpora_path):
             f2.write(' '.join(slice))
 
 
-#split_corpora('/Users/mariabocharova/PycharmProjects/Thesis/corpora.txt')
-
-
 # Считаем коэффициент Жуайна
-def juilland_d(term, root_path):
+def juilland_d(term):
+    root_path = '/Users/mariabocharova/PycharmProjects/Thesis/split_corpora'
     # n – количество сегментов, на которое развит корпус
     # μ – средняя частота слова по всему корпусу (mfrq)
     # σ – среднее квадратичное отклонение частоты μ на отдельных сегментах (rmse)
     segment_paths = [elem for elem in os.listdir(root_path) if not elem.startswith('.')]
     n = len(segment_paths)
-    segment_freq = []
-    for path in tqdm(segment_paths):
+    freq = []
+    freq_num = []
+    for path in segment_paths:
         with open(f'{root_path}/{path}') as f:
-            segment_freq.append(f.read().count(term))
-    rmse = np.std(segment_freq)
-    mfrq = sum(segment_freq) / n
-    jd = round(100 * (1 - (rmse / (mfrq * sqrt(n - 1)))), 3)
-    r = (len(segment_freq)
-    return jd, r
+            num = f.read().count(term)
+            freq.append(num)
+            if num > 0:
+                freq_num.append(num)
+    rmse = np.std(freq)
+    mfrq = sum(freq) / n
+    jd = round(100 * (1 - (rmse / (mfrq * sqrt(n - 1)))), 2)
+    r = len(freq_num)
+    return pd.Series([jd, r])
 
 
-print(juilland_d('язык', '/Users/mariabocharova/PycharmProjects/Thesis/split_corpora'))
+s = []
+root_path = '/Users/mariabocharova/PycharmProjects/Thesis/split_corpora'
+segment_paths = [elem for elem in os.listdir(root_path) if not elem.startswith('.')]
+for path in tqdm(segment_paths):
+    with open(f'{root_path}/{path}') as f:
+        s.append(len(f.read().split()))
+sum_s = sum(s)
+for i in range(len(s)):
+    s[i] = round(s[i] / sum_s, 3)
+with open('s.pickle', 'wb') as f:
+    pickle.dump(s, f)
 
 
-'''path = '/Users/mariabocharova/PycharmProjects/Thesis/lemmatization/frequency_dict_full.csv'
-tqdm.pandas()
-df = pd.read_csv(path)
-df['R'] = df['термин'].progress_apply()
-df['D'] = df['термин'].progress_apply()
+# Deviation of Proportions
+def dp(term):
+    global s, segment_paths
+    """
+    n = 5: the length of the corpus in parts
+    v = (1, 2, 3, 4, 5): the frequencies of a word in each corpus part 1-n
+    f = 15: (he overall frequency of a word in the corpus
+    s = (0.18, 0.2, 0.2, 0.2, 0.22): the percentages of the n corpus part sizes
+    """
+    n = len(segment_paths)
+    v = []
+    for path in segment_paths:
+        with open(f'{root_path}/{path}') as f:
+            text = f.read()
+            num = text.count(term)
+            v.append(num)
+    sum_v = sum(v)
+    for i in range(len(v)):
+        v[i] = round(v[i] / sum_v, 3)
+    f = sum(v)
+    dp = 0
+    for i in range(n):
+        dp += 0.5 * (abs((v[i] / f) - s[i]))
+    return round(dp, 4)
 
-juilland_d('/Users/mariabocharova/PycharmProjects/Thesis/split_corpora')
-'''
+
+with open('s.pickle', 'rb') as f:
+    s = pickle.load(f)
+
+root_path = '/Users/mariabocharova/PycharmProjects/Thesis/split_corpora'
+segment_paths = [elem for elem in os.listdir(root_path) if not elem.startswith('.')]
+
+paths = ['/Users/mariabocharova/PycharmProjects/Thesis/lemmatization/frequency_dict.csv',
+         '/Users/mariabocharova/PycharmProjects/Thesis/lemmatization/bigram_frequency_dict.csv',
+         '/Users/mariabocharova/PycharmProjects/Thesis/lemmatization/trigram_frequency_dict.csv',
+         '/Users/mariabocharova/PycharmProjects/Thesis/lemmatization/fourgram_frequency_dict.csv',
+         '/Users/mariabocharova/PycharmProjects/Thesis/lemmatization/fivegram_frequency_dict.csv']
+
+for path in paths:
+    df = pd.read_csv(path)
+    tqdm.pandas()
+    df[['DP']] = df['термин'].progress_apply(dp)
+    tqdm.pandas()
+    df[['D', 'R']] = df['термин'].progress_apply(juilland_d)
+    df.to_csv(f'{path[:-4]}_2.csv')
